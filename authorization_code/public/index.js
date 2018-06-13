f39cfff28a4c150aa7b4f8059648b474e89edce1
Your Name <you@example.com>
2018-06-13 18:23:28 +0200

    change

--- authorization_code/public/index.js
+++ authorization_code/public/index.js
@@ -11,6 +11,8 @@ $(document).ready(function () { //aspetto che il documento sia pronto
         return hashParams;
     }
 
+    
+
     var params = getHashParams(); 
     var access_token = params.access_token,
         refresh_token = params.refresh_token,
